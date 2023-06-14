4ca629313a6d998ec70a80d22ece759cd0e911c6
Oliver Schmitz <mail@ollischmitz.net>
2023-06-14 17:51:48 +0200

    fix inital database script

--- applications/server-app/src/main/resources/db/changelog/version-1.0.sql
+++ applications/server-app/src/main/resources/db/changelog/version-1.0.sql
@@ -1,9 +1,23 @@
 -- Initial database setup Version 1.0
+-- Clean Up
+DROP TABLE IF EXISTS "Person" CASCADE ;
+DROP TABLE IF EXISTS "User" CASCADE ;
+DROP TABLE IF EXISTS "Acquaintance" CASCADE ;
+DROP TYPE IF EXISTS "sex" CASCADE ;
 -- Enable UUID
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Custom Types
 CREATE TYPE sex AS ENUM('MALE','FEMALE', 'DIVERSE', 'INDEFINITE');
 
+CREATE TABLE "Person" (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at timestamp DEFAULT now(),
+    ---
+    name varchar NOT NULL,
+    day_of_birth date NOT NULL,
+    height numeric(3,2) NOT NULL,
+    sex sex DEFAULT 'INDEFINITE'
+);
 CREATE TABLE "User" (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     created_at timestamp DEFAULT now(),
@@ -14,18 +28,7 @@ CREATE TABLE "User" (
     person_id uuid references "Person"(id)
 );
 
-CREATE TABLE "Person" (
-    id uuid DEFAULT uuid_generate_v4(),
-    created_at timestamp DEFAULT now(),
-    ---
-    name varchar NOT NULL,
-    day_of_birth date NOT NULL,
-    height numeric(3,2) NOT NULL,
-    sex sex DEFAULT 'INDEFINITE'
-);
-
 CREATE TABLE "Acquaintance" (
     user_id uuid references "User"(id),
-    person_id uuid references "Person"(id),
-    relation varchar
+    person_id uuid references "Person"(id)
 )
